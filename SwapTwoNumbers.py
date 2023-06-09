4fb9179ca14d9da7af233b2782a0243a6c786562
ChaitanyaTG <70212909+ChaitanyaTG@users.noreply.github.com>
2023-06-09 13:53:01 +0530

    Add files via upload

---
+++ SwapTwoNumbers.py
@@ -0,0 +1,14 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+
+def swap(a, b):
+    temp = 0
+    temp = a
+    a = b
+    b = temp
+
+    return a, b
+    pass
